9f02af39a06981023815108debd460b5864ffd59
T <None>
2019-08-14 20:29:27 +0300

    Rewrote ast.h
    based on the template (check the comments)

--- 3. Semantics Analysis/ast.h
+++ 3. Semantics Analysis/ast.h
@@ -9,130 +9,333 @@
 #ifndef __AST_H__
 #define __AST_H__
 
-// Operations
-typedef struct ast_node{
-	Operation op;
-	Type type;
-	
-	AST_Node *left;
-	AST_Node *right;
-}AST_Node;
-
-// If's
-typedef struct ast_if_node{
-	Operation op;
-	
-	AST_Node *condition;
-	AST_Node *if_branch;
-	AST_Node *else_brach;
-} AST_IF_Node;
-
-// While Loop
-typedef struct ast_while_node{
-	Operation op;
-	
-	AST_Node *condition;
-	AST_Node *while_branch;
-}AST_WHILE_Node;
-
-// For loop
-typedef struct ast_for_node{
-	Operation op;
-
-	//(i=0; i<10; i++)
-	AST_Node *start_condition;
-	AST_Node *end_condition;
-	int increment;
-
-	//{
-
-	//}
-	AST_Node *for_branch;
-}AST_FOR_Node;
 
 
-//declare variable
-typedef struct ast_decl_node
-{
-    Type variable_type;
-    char *id;
-    int dim;
-	
-	//initial value?
+/* ---------------------------------------------------------------------
+   ------------------------- Ορισμός τύπων -----------------------------
+   --------------------------------------------------------------------- */
 
-}AST_DECL_Node;
+/* Δηλώσεις */
 
-//Leaf for static or ids
-typedef struct ast_leaf_node{
-	Operation op;
-	char *name;
+typedef struct AST_program_tag * AST_program;
+typedef struct AST_letdef_tag  * AST_letdef;
+typedef struct AST_typedef_tag * AST_typedef;
+typedef struct AST_def_tag     * AST_def;
+typedef struct AST_tdef_tag    * AST_tdef;
+typedef struct AST_constr_tag  * AST_constr;
+typedef struct AST_par_tag     * AST_par;
+typedef struct AST_expr_tag    * AST_expr;
+typedef struct AST_clause_tag  * AST_clause;
+typedef struct AST_pattern_tag * AST_pattern;
 
-	Type_Struct *value;
-}AST_LEAF_Node;
+typedef struct AST_ltdef_list_tag   * AST_ltdef_list;
+typedef struct AST_def_list_tag     * AST_def_list;
+typedef struct AST_tdef_list_tag    * AST_tdef_list;
+typedef struct AST_constr_list_tag  * AST_constr_list;
+typedef struct AST_par_list_tag     * AST_par_list;
+typedef struct AST_expr_list_tag    * AST_expr_list;
+typedef struct AST_clause_list_tag  * AST_clause_list;
+typedef struct AST_pattern_list_tag * AST_pattern_list;
 
-
-typedef struct ast_enum_leaf_node{
-	Operation op;
-	char *name;
-
-	Type_Struct *values;
-	Type_Struct *enumeration;
-}AST_ENUM_LEAF_Node;
-
-
-typedef struct function{
-	Operation op;
-	char *name;
-
-	AST_Node *arguments;
-	AST_Node *statements;
-	Type_Struct *return_type;
-
-}Function;
-
-typedef struct class_member{
-    char *name;
-    Type_Struct *member_type;
-    struct Class_Member *next;
-}Class_Member;
-
-typedef struct class_method{
-    char *name;
-    Type_Struct return_type;
-    struct AST_Node *arguments;
-	// Func / execute method code
-	struct AST_Node *statements;
-    struct Class_Method *next;
-}Class_Method;
+typedef struct Type_list_tag * Type_list;
 
 
-typedef struct class{
-	Operation op;
+/* Απαριθμήσεις τελεστών */
 
-    char *name;
-    Class_Member *members;
-	int no_members;
-	Class_Method *methods;
-	int no_methods;
-}Class;
 
-AST_Node *create_ast_node(Operation op, Type type, AST_Node *left, AST_Node *right);
-AST_Node *create_ast_if_node(AST_Node *condition, AST_Node *if_branch, AST_Node *else_branch);
-AST_Node *create_ast_while_node(AST_Node *condition, AST_Node *while_branch);
-AST_Node *create_ast_for_node(AST_Node *start_condition, AST_Node *end_condition, int increment, AST_Node *for_branch);
-AST_Node *create_leaf_node(Type_Struct *value, char *name);
-AST_Node *create_function_node(char *name, AST_Node *arguments, AST_Node *statements, Type_Struct *return_type);
+typedef enum{
+	ast_unop_plus,
+	ast_unop_minus,
+	ast_unop_not,
+	ast_unop_inc,
+	ast_unop_dec,
+	//might need to add more	
+}AST_unop;
 
-AST_DECL_Node* new_single_decl_node(Type variable_type, char* id);
-AST_Node* new_asgn_node(AST_Node* variable,AST_Node* value);
-AST_Node* new_exp_node(Operation op_type , AST_Node* left_exp , AST_Node* right_exp);
-AST_Node* new_condition_node(Operation op_type,AST_Node* expr1,AST_Node* expr2);
+typedef enum {
+    ast_binop_plus,
+    ast_binop_minus,
+    ast_binop_times,
+    ast_binop_div,
+    ast_binop_fplus,
+    ast_binop_fminus,
+    ast_binop_ftimes,
+    ast_binop_fdiv,
+    ast_binop_mod,
+    ast_binop_exp,
+    ast_binop_eq,
+    ast_binop_ne,
+    ast_binop_lt,
+    ast_binop_gt,
+    ast_binop_le,
+    ast_binop_ge,
+    ast_binop_pheq,   //do we need this ?
+    ast_binop_phne,	  //do we need this ?
+    ast_binop_and,
+    ast_binop_or,
+    ast_binop_semicolon,
+    ast_binop_assign
+} AST_binop;
 
-//these might become one
-AST_Node* new_iconst_node(int data);
-AST_Node* new_fconst_data(float data);
-AST_Node* new_cconst_node(char data);
-AST_Node* new_sconst_node(char* data);
+//First symbol of c++600
+typedef struct AST_program_tag{
+	AST_letdef_list list;
+	int lineno;
+};
+
+//change name ?
+struct AST_letdef_tag{
+	bool recFlag; //do we need this?
+	AST_def_list list;
+	int lineno;
+};
+
+struct AST_def_tag {
+    enum {
+        DEF_normal,
+        DEF_mutable,
+    } kind;
+    union {
+        struct {
+            Identifier id;
+            AST_par_list list;
+            Type type;
+            AST_expr expr;
+        } d_normal;
+        struct {
+            Identifier id;
+            AST_expr_list list;
+            Type type;
+        } d_mutable; // change name?
+    } u;
+    int lineno;
+    SymbolEntry entry;
+};
+
+struct AST_tdef_tag {
+    Identifier id;
+    AST_constr_list list;
+    int lineno;
+};
+
+struct AST_constr_tag {
+    Identifier id;
+    Type_list list;
+    int lineno;
+};
+
+struct AST_par_tag {
+    Identifier id;
+    Type type;
+    int lineno;
+};
+
+struct AST_expr_tag {
+    enum {
+        EXPR_iconst,
+        EXPR_fconst,
+        EXPR_cconst,
+        EXPR_strlit, // do we need this?
+        EXPR_true,
+        EXPR_false,
+        EXPR_unit,
+        EXPR_unop,
+        EXPR_binop,
+        EXPR_id,
+        EXPR_Id,
+        //EXPR_call,
+        //EXPR_Call,
+       // EXPR_arrel,
+        EXPR_dim,
+        EXPR_new,
+        EXPR_delete,
+        //EXPR_let,
+        EXPR_if,
+        EXPR_while,
+        EXPR_for,
+        //EXPR_match, //check what we need and what we dont
+        EXPR_class
+    } kind;
+    union {
+        struct {
+            int rep;
+        } e_iconst;
+        struct {
+            float rep;
+        } e_fconst;
+        struct {
+            char rep;
+        } e_cconst;
+        struct {
+            char* rep;
+        } e_strlit;
+        struct {
+            AST_unop op;
+            AST_expr expr;
+        } e_unop;
+        struct {
+            AST_expr expr1;
+            AST_binop op;
+            AST_expr expr2;
+        } e_binop;
+        struct {
+            Identifier id;
+        } e_id;
+        struct {
+            Identifier id;
+        } e_Id;
+        struct {
+            Identifier id;
+            AST_expr_list list;
+        } e_call;
+        struct {
+            Identifier id;
+            AST_expr_list list;
+        } e_Call;
+        struct {
+            Identifier id;
+            AST_expr_list list;
+        } e_arrel;
+        struct {
+            int dim;
+            Identifier id;
+        } e_dim;
+        struct {
+            Type type;
+        } e_new;
+        struct {
+            AST_expr expr;
+        } e_delete;
+        struct {
+            AST_letdef def;
+            AST_expr expr;
+        } e_let;
+        struct {
+            AST_expr econd;
+            AST_expr ethen;
+            AST_expr eelse;
+        } e_if;
+        struct {
+            AST_expr econd;
+            AST_expr ebody;
+        } e_while;
+        struct {
+            Identifier id;
+            AST_expr expr1;
+            bool downFlag;
+            AST_expr expr2;
+            AST_expr ebody;
+        } e_for;
+        struct {
+            AST_expr expr;
+            AST_clause_list list;
+        } e_match;
+        struct{
+            Identifier name;            //Class Name
+            AST_expr_list members;          // ex: int i , char c
+            AST_expr_list methods;      // doSomething()
+        }e_class;
+    } u;
+    int lineno;
+    Type type;
+    SymbolEntry entry;
+};
+
+struct AST_clause_tag {
+    AST_pattern pattern;
+    AST_expr expr;
+    int lineno;
+};
+
+struct AST_pattern_tag {
+    enum {
+        PATTERN_iconst, //do we need all the consts?
+        PATTERN_fconst,
+        PATTERN_cconst, 
+        PATTERN_true,
+        PATTERN_false,
+        PATTERN_id,
+        PATTERN_Id
+    } kind;
+    union {
+        struct {
+            int rep;
+        } p_iconst;
+        struct {
+            float rep;
+        } p_fconst;
+        struct {
+            char rep;
+        } p_cconst;
+        struct {
+            Identifier id;
+        } p_id;
+        struct {
+            Identifier id;
+            AST_pattern_list list;
+        } p_Id;
+    } u;
+    int lineno;
+};
+
+
+AST_program ast_program           (AST_ltdef_list l);
+AST_letdef  ast_letdef            (bool recFlag, AST_def_list l); 		  //name?
+AST_def     ast_def_normal        (Identifier id, AST_par_list l,
+        Type t, AST_expr e);
+AST_def     ast_def_mutable       (Identifier id, AST_expr_list l, Type t); //name?
+AST_typedef ast_typedef           (AST_tdef_list l);
+AST_tdef    ast_tdef              (Identifier id, AST_constr_list l);
+AST_constr  ast_constr            (Identifier id, Type_list l);
+AST_par     ast_par               (Identifier id, Type t);
+AST_clause  ast_clause            (AST_pattern p, AST_expr e);
+
+AST_expr ast_expr_iconst (int r);
+AST_expr ast_expr_fconst (float r);
+AST_expr ast_expr_cconst (char r);
+//AST_expr ast_expr_strlit (char* r);
+AST_expr ast_expr_true   ();
+AST_expr ast_expr_false  ();
+AST_expr ast_expr_unit   ();
+AST_expr ast_expr_unop   (AST_unop op, AST_expr e);
+AST_expr ast_expr_binop  (AST_expr e1, AST_binop op, AST_expr e2);
+AST_expr ast_expr_id     (Identifier id);
+AST_expr ast_expr_Id     (Identifier id);
+// AST_expr ast_expr_call   (Identifier id, AST_expr_list l);
+// AST_expr ast_expr_Call   (Identifier id, AST_expr_list l);
+// AST_expr ast_expr_arrel  (Identifier id, AST_expr_list l);
+AST_expr ast_expr_dim    (int dim, Identifier id);
+AST_expr ast_expr_new    (Type t);
+AST_expr ast_expr_delete (AST_expr e);
+AST_expr ast_expr_let    (AST_letdef ld, AST_expr e);
+AST_expr ast_expr_if     (AST_expr ec, AST_expr e1, AST_expr e2);
+AST_expr ast_expr_while  (AST_expr ec, AST_expr eb);
+AST_expr ast_expr_for    (Identifier id, AST_expr e1, bool down,
+        AST_expr e2, AST_expr eb);
+// AST_expr ast_expr_match  (AST_expr e, AST_clause_list l);
+AST_expr ast_expr_class(Identifier id, Type_list tl, AST_expr_list el);
+
+
+AST_pattern ast_pattern_iconst (int r);
+AST_pattern ast_pattern_fconst (float r);
+AST_pattern ast_pattern_cconst (char r);
+AST_pattern ast_pattern_true   ();
+AST_pattern ast_pattern_false  ();
+AST_pattern ast_pattern_id     (Identifier id);
+AST_pattern ast_pattern_Id     (Identifier id, AST_pattern_list l);
+
+AST_ltdef_list   ast_ltdef_list_let  (AST_letdef ld  , AST_ltdef_list l);
+AST_ltdef_list   ast_ltdef_list_type (AST_typedef td , AST_ltdef_list l);
+
+AST_def_list     ast_def_list        (AST_def d      , AST_def_list l);
+AST_tdef_list    ast_tdef_list       (AST_tdef td    , AST_tdef_list l);
+AST_constr_list  ast_constr_list     (AST_constr c   , AST_constr_list l);
+AST_par_list     ast_par_list        (AST_par p      , AST_par_list l);
+AST_expr_list    ast_expr_list       (AST_expr e     , AST_expr_list l);
+AST_clause_list  ast_clause_list     (AST_clause c   , AST_clause_list l);
+AST_pattern_list ast_pattern_list    (AST_pattern p  , AST_pattern_list l);
+
+Type_list type_list (Type t, Type_list l);
 
 #endif
 
